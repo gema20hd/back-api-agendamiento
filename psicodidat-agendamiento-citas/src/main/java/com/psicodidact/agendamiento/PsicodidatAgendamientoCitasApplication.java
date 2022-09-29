66f2a98eff37099b64371121e7e652fc13385c48
gema20hd <gemacostzhd@hotmail.com>
2022-09-29 00:15:18 -0500

    proyecto base

---
+++ psicodidat-agendamiento-citas/src/main/java/com/psicodidact/agendamiento/PsicodidatAgendamientoCitasApplication.java
@@ -0,0 +1,14 @@
+package com.psicodidact.agendamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsicodidatAgendamientoCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsicodidatAgendamientoCitasApplication.class, args);
+		System.out.println ("Hola Mundo");
+	}
+
+}
---
+++ psicodidat-agendamiento-citas/src/test/java/com/psicodidat/agendamiento/PsicodidatAgendamientoCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psicodidat.agendamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsicodidatAgendamientoCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
